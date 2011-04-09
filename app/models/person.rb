9138d1464c61952bc8adb4d388f46129f9d2fa2d
Pedro Nascimento <pnascimento@gmail.com>
2011-04-09 19:35:49 -0300

    adding without_ methods to exclude records from queries

--- app/models/person.rb
+++ app/models/person.rb
@@ -8,5 +8,12 @@ class Person < ActiveRecord::Base
       formatted_string = '%%%{email}' % {:email => string}
       Person.where('email LIKE ?', formatted_string)
     end
+    def without_email(string)
+      formatted_string = '%%%{email}' % {:email => string}
+      Person.where('email NOT LIKE ?', formatted_string)
+    end
+    def without_postcode(numbers)
+      joins(:postcode).where('postcodes.number NOT IN (?)', numbers)
+    end
   end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -13,6 +13,16 @@ describe Person do
       postcode = Postcode.find(1,2)
       Person.with_postcode([postcode[0].number, postcode[1].number]).count.should == 2
     end
+
+    it 'should fetch records given an excluding postcode' do
+      person = Factory(:person, :postcode => Factory(:postcode, :number => 2000))
+      Person.without_postcode(2000).should_not include person
+    end
+    it 'should fetch records given multiple excluding postcodes' do
+      3.times { Factory :person}
+      postcode = Postcode.find(1,2)
+      Person.without_postcode([postcode[0].number, postcode[1].number]).count.should == 1
+    end
   end
 
   context 'searching for email addresses' do
@@ -20,6 +30,11 @@ describe Person do
     it 'should fetch records given an email domain' do
       email_domain = 'hotmail.com'
       Person.with_email(email_domain).should include person
+    end
+
+    it 'should fetch records given an excluding email domain' do
+      email_domain = 'hotmail.com'
+      Person.without_email(email_domain).should_not include person
     end
   end
 end
